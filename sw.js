adf5856394e31f595e115a4b191941bbc2398b60
DESKTOP-B42QI1T\user <3144347657@qq.com>
2026-08-10 17:20:54 +0900

    Site updated: 2026-08-10 17:20:53

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1785751990179";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1786349860963";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
